7de75b8434f47d4d4f8b1010d65c6bd8a618c63b
mjseok <mjseok98@gmail.com>
2022-08-15 23:41:28 +0900

    feat:Add BOJ_2178-java

---
+++ Java/src/com/ssafy/solvedac/BOJ_2178.java
@@ -0,0 +1,57 @@
+package com.ssafy.solvedac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_2178 {
+	static int N, M;
+	static int[][] miro;
+	static int[] dx = { 0, 0, -1, 1 };
+	static int[] dy = { -1, 1, 0, 0 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String str = in.readLine();
+		StringTokenizer st = null;
+
+		st = new StringTokenizer(str, " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		miro = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			str = in.readLine();
+			for (int j = 0; j < M; j++) {
+				miro[i][j] = str.charAt(j)-'0';
+			}
+		}
+		bfs(0,0);
+		System.out.println(miro[N-1][M-1]);
+	}
+
+	private static void bfs(int x, int y) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] { x, y});
+		while (!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			int curX = cur[0];
+			int curY = cur[1];
+			for (int d = 0; d < 4; d++) {
+				int nx = curX + dx[d];
+				int ny = curY + dy[d];
+				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+					continue;
+				if (miro[nx][ny] ==1) {
+					miro[nx][ny] = 0;
+					queue.add(new int[] { nx, ny});
+
+					miro[nx][ny]=miro[curX][curY]+1;
+				}
+			}
+
+		}
+	}
+}
