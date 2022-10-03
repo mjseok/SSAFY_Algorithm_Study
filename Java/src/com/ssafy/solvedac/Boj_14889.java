5e24b0a4f9da891dcb2d978200e47da8a2660816
mjseok <mjseok98@gmail.com>
2022-10-04 00:30:53 +0900

    feat:Add Boj_14889

---
+++ Java/src/com/ssafy/solvedac/Boj_14889.java
@@ -0,0 +1,73 @@
+package com.ssafy.solvedac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj_14889 {
+	static int[] start;
+	static int[] link;
+	static int half;
+	static int N;
+	static boolean[] visited;
+	static int min= Integer.MAX_VALUE;
+	static int[] people;
+	static int[][] map;
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(in.readLine());
+		half = N/2;
+		start = new int[half];
+		visited = new boolean[N];
+		people = new int[N];
+		for(int i=0;i<N;i++) {
+			people[i]=i;
+		}
+		
+		map = new int[N][N];
+		StringTokenizer st = null;
+		for(int i=0;i<N;i++) {
+			st= new StringTokenizer(in.readLine()," ");
+			for(int j=0;j<N;j++) {
+				map[i][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+		ncr(0,0);
+		System.out.println(min);
+	}
+	private static void ncr(int s, int cnt) {
+		if(cnt==half) {
+			cal(start);
+			//System.out.println(Arrays.toString(start));
+			return;
+		}
+		for(int i=s;i<N;i++) {
+			visited[i]=true; //선택, 안선택한게 뭔지 알고 싶을 때를 위해서는 남겨둘 수 있음
+			start[cnt] = people[i];
+			ncr(i+1,cnt+1);
+			start[cnt]=0;//선택, 안선택한게 뭔지 알고 싶을 때를 위해서는 남겨둘 수 있음
+			visited[i] = false; //선택, 안선택한게 뭔지 알고 싶을 때를 위해서는 남겨둘 수 있음
+		}
+	}
+	private static void cal(int[] start) {
+		int startPower = 0;
+		int linkPower = 0;
+		for(int i=0;i<N-1;i++) {
+			for(int j=i+1;j<N;j++) {
+				if(visited[i] == true &&visited[j]==true) {
+					//start팀
+					startPower+=map[i][j];
+					startPower+=map[j][i];
+				}else if(visited[i]==false && visited[j]==false) {
+					linkPower+=map[i][j];
+					linkPower+=map[j][i];
+				}
+			}
+		}
+		int diff = Math.abs(startPower-linkPower);
+		min = diff<min ? diff:min;
+		
+	}
+}
